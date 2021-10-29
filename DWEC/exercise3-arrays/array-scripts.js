45650ad682ad15c16af73c1dc24bf57d89ca5f87
Toni <garcia.t20@gmail.com>
2021-10-29 21:07:12 +0200

    all working. Missing number 10 and 14

--- DWEC/exercise3-arrays/array-scripts.js
+++ DWEC/exercise3-arrays/array-scripts.js
@@ -54,7 +54,7 @@ function sortingArrayMinorToMajor(){
 //7
 function sortingArrayMajorToMinor(){
     let newArray = document.getElementById("main-content").value.split(",");
-    let minorToMajorOrder = (a,b) => {
+    let minorToMajorOrder = (a, b) => {
         return b-a;
     }
     newArray.sort(minorToMajorOrder)
@@ -64,28 +64,96 @@ function sortingArrayMajorToMinor(){
 //8
 function orderByMajorLength() {
     let newArray = document.getElementById("main-content").value.split(",");
-    let minorToMajorOrder = (a,b) => {
-        newArray.forEach(index, value, array){
-
+    let majorLength = (a, b) => {
+        return b.length - a.length;
         }
-
-        return b-a;
-    }
-    newArray.sort(minorToMajorOrder)
+    newArray.sort(majorLength)
     document.getElementById("result-tag").innerHTML = newArray;
 }
 
-//10 NOT WORKING!
+//9
+function arrayOfLengths(){
+    let newArray = document.getElementById("main-content").value.split(",");
+    let newArrayLengths = newArray.map((item) => { return item.length; });
+    document.getElementById("result-tag").innerHTML = newArrayLengths;
+}
+
+// 10 NOT WORKING! //
 function separatedByMidDash() {
     let newArray = document.getElementById("main-content").value.split(",");
-    let chainWithTheArray = "";
-    let concatenate = (item, index, array) => {
-        chainWithTheArray += item;
-        array[index].trim(); // I want to clean the spaces but this is not working
-        if(index !== array.length-1) {
-            chainWithTheArray += "-";
+}
+
+//11
+function acronym() {
+    let newArray = document.getElementById("main-content").value.split(",");
+    let newArrayAcronym = newArray.map((item) => {
+        return item.split("")[0].toUpperCase()
+    });
+    document.getElementById("result-tag").innerHTML = newArrayAcronym.toString().replaceAll(",", "");
+}
+
+//12
+function deletingMinorOf5Length() {
+    let newArray = document.getElementById("main-content").value.split(",");
+    document.getElementById("result-tag").innerHTML = newArray.filter((item) => {
+        return item.length >= 5;
+    });
+}
+
+//13
+function sumItemsForEach() {
+    let newArray = document.getElementById("main-content").value.split(",");
+    let total = 0;
+    newArray.forEach( value => { total += parseInt(value); });
+    document.getElementById("result-tag").innerHTML = total;
+}
+
+//14
+function laInArray() {
+    let newArray = document.getElementById("main-content").value.split(",");
+
+}
+
+//15
+function sumItemsReduce() {
+    let newArray = document.getElementById("main-content").value.split(",");
+    document.getElementById("result-tag").innerHTML = newArray.reduce( (total, value ) => { return parseInt(total) + parseInt(value)});
+}
+
+//16
+function maxNum() {
+    let newArray = document.getElementById("main-content").value.split(",");
+    let max = Number.MIN_SAFE_INTEGER;
+    document.getElementById("result-tag").innerHTML = newArray.forEach( value => {
+        if (parseInt(value) > max) {
+            max = parseInt(value);
         }
+    });
+    document.getElementById("result-tag").innerHTML = max;
+}
+
+//17
+function arrayRestructuring() {
+    let newArray = document.getElementById("main-content").value.split(",");
+    if (newArray.length < 3) {
+        document.getElementById("result-tag").innerHTML = "Introducir al menos 3 valores";
+    } else if (newArray.length %2 !== 0){
+        document.getElementById("result-tag").innerHTML = "El total de valores a introducir ha de ser par (6,8,10,...)";
+    } else {
+        newArray.splice(newArray.length/2, 0, newArray[0], newArray[newArray.length-1]);
+        newArray.splice(0,1);
+        newArray.splice(newArray.length-1,1);
+        document.getElementById("result-tag").innerHTML = newArray;
     }
-    newArray.forEach(concatenate);
-    document.getElementById("result-tag").innerHTML = chainWithTheArray;
+}
+
+//18
+function capCuaArray(){
+    let newArray = document.getElementById("main-content").value.split(",");
+    if (newArray.length < 3) {
+        document.getElementById("result-tag").innerHTML = "Debes enviar al menos 3 números para realizar la operación";
+    } else {
+        newArray.splice(newArray.length, 0, newArray[2], newArray[1], newArray[0]).splice(0, 3);
+        document.getElementById("result-tag").innerHTML = newArray;
+    }
 }
