336da7d3a6413b9ff995a8bfb7dfe25fde09a021
ToniGar20 <tgarcia@habitissimo.com>
2021-11-29 11:50:16 +0100

    exercise 6 and 7

--- DWEC/exercise4-objects/scripts.js
+++ DWEC/exercise4-objects/scripts.js
@@ -77,7 +77,7 @@ document.addEventListener("click", (e) => {
                     "nom": item.nom,
                     "born": item.born,
                     "height": item.height,
-                    "tours": item.tours = item.tours+1,
+                    "tours": item.tours = item.tours + 1,
                     "wins": item.wins
                 };
             }
@@ -99,15 +99,61 @@ document.addEventListener("click", (e) => {
             break;
 
         case 6:
-            alert("helo");
+            function oldCyclist(item){
+                function getAge(dateString) {
+                    let today = new Date();
+                    let birthDate = new Date(dateString);
+                    let age = today.getFullYear() - birthDate.getFullYear();
+                    let m = today.getMonth() - birthDate.getMonth();
+                    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                        age--;
+                    }
+                    return age;
+                }
+                return getAge(item.born) > 30;
+            }
+
+            console.log(infoCiclistes);
+            let noOldCyclist = infoCiclistes.filter(oldCyclist);
+            console.log(noOldCyclist);
+
+            result = "";
+            for (let i = 0; i < parseInt(noOldCyclist.length); i++) {
+                result += noOldCyclist[i]["nom"];
+                if (i === parseInt(noOldCyclist.length)-1) {
+                    result += ".";
+                } else {
+                    result += ", "
+                }
+            }
+            document.getElementById("result-tag").innerHTML = result;
             break;
 
         case 7:
-            alert("helo");
+        function majorthan30(item){
+            function getAge(dateString) {
+                let today = new Date();
+                let birthDate = new Date(dateString);
+                let age = today.getFullYear() - birthDate.getFullYear();
+                let m = today.getMonth() - birthDate.getMonth();
+                if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                    age--;
+                }
+                return age;
+            }
+            return getAge(item.born) > 30;
+        }
+
+            console.log(infoCiclistes);
+            let firstMajorThan30 = infoCiclistes.find(majorthan30);
+            console.log(firstMajorThan30);
+
+            result = firstMajorThan30["nom"];
+            document.getElementById("result-tag").innerHTML = result;
             break;
 
         case 8:
-            alert("helo");
+
             break;
 
         case 9:
