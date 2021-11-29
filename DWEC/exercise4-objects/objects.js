b81ca5a9ab5bf90debd7567e438b33ba3403024f
ToniGar20 <tgarcia@habitissimo.com>
2021-11-29 13:12:31 +0100

    formatting date to correct string format: YYYY-MM-DD

--- DWEC/exercise4-objects/objects.js
+++ DWEC/exercise4-objects/objects.js
@@ -3,42 +3,42 @@ export let equip_ciclista = {
     ciclistes: [
         {
             nom: "Juan Alonso",
-            born: "8/01/1958",
+            born: "1958-01-08",
             height: 1.92,
             tours: 0,
             wins: 12,
         },
         {
             nom: "Josh Pantano",
-            born: "25/10/2000",
+            born: "2000-10-25",
             height: 1.84,
             tours: 3,
             wins: 12,
         },
         {
             nom: "Bid Seville",
-            born: "12/07/1993",
+            born: "1993-07-12",
             height: 1.88,
             tours: 3,
             wins: 3
         },
         {
             nom: "Zolz Brucker",
-            born: "08/09/1984",
+            born: "1984-09-08",
             height: 1.72,
             tours: 0,
             wins: 1,
         },
         {
             nom: "Otto Verdes",
-            born: "19/2/1979",
+            born: "1979-02-19",
             height: 1.92,
             tours: 0,
             wins: 12,
         },
         {
             nom: "Bert Poel",
-            born: "03/06/1995",
+            born: "1995-06-03",
             height: 1.99,
             tours: 1,
             wins: 2,
